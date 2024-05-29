28acc8fa040c346dacfe70fc1ed4f879f33829be
haden <80521474+thdefn@users.noreply.github.com>
2024-05-30 01:15:00 +0900

    fix: fix the error post comments

--- community/src/main/java/twotwo/community/dto/request/CommentRequest.java
+++ community/src/main/java/twotwo/community/dto/request/CommentRequest.java
@@ -1,9 +1,13 @@
 package twotwo.community.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentRequest {
     @NotBlank
     private String content;
--- community/src/main/java/twotwo/community/dto/request/PostRequest.java
+++ community/src/main/java/twotwo/community/dto/request/PostRequest.java
@@ -2,10 +2,14 @@ package twotwo.community.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import twotwo.community.domain.PostType;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostRequest{
     @NotNull
     private PostType postType;
