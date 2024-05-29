be8489d9656098e5e3c999f56d98f3ebf6d96bfe
haden <80521474+thdefn@users.noreply.github.com>
2024-05-30 00:45:25 +0900

    fix: fix the error retrieve posts

--- community/src/main/java/twotwo/community/post/service/AnswerService.java
+++ community/src/main/java/twotwo/community/post/service/AnswerService.java
@@ -129,7 +129,7 @@ public class AnswerService {
     }
 
     public List<AnswerResponse> retrieve(String postId, String token){
-        if(!postRepository.findByIdAndType(postId, PostType.QUESTION))
+        if(!postRepository.findByIdAndType(postId, PostType.QNA))
             throw new BudException(NOT_FOUND_POST);
         Long userId = tokenProvider.getId(token);
         List<Answer> answers = answerRepository.findByPostId(postId);
